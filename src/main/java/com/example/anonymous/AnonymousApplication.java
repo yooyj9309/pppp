ded6a074efb33ae648a8db116af23a934ee2f1af
yooyj9309 <yooyj9309@zuminternet.com>
2018-10-04 17:02:26 +0900

    first commit

---
+++ src/main/java/com/example/anonymous/AnonymousApplication.java
@@ -0,0 +1,12 @@
+package com.example.anonymous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/anonymous/AnonymousApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.anonymous;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnonymousApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
